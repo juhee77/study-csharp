5a60cbb7461d2fc221ef4312a231c52784d91e8d
lahezy <jouhi3322@gmail.com>
2025-08-31 22:32:15 +0900

    feat : 변수와 반복문

--- headfirst-4th-edition/01-getting-started/MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ headfirst-4th-edition/01-getting-started/MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -6,10 +6,50 @@ namespace MyFirstConsoleApp
     {
         static void Main(string[] args)
         {
+            OperatorExamples();
+            OperatorExamples2();
+        }
+
+        public static void OperatorExamples()
+        {
+
             Console.WriteLine("Hello, World!");
             Console.WriteLine("This is my first console application in C#.");
             Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+
+            int width = 3;
+            width++;
+            int height = 2 + 4;
+            int area = width * height;
+            Console.WriteLine(area);
+
+            string result = "The area";
+            result = result + " is " + area;
+            Console.WriteLine(result);
+
+            bool truthValue = true;
+            Console.WriteLine(truthValue);
+
+        }
+
+        public static void OperatorExamples2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            int hold = 0;
+            while (hold < 5)
+            {
+                Console.WriteLine("hold : " + hold++);
+            }
+
+            do
+            {
+                Console.WriteLine("while hold " + hold++);
+            } while (hold < 10);
         }
     }
 }
+
